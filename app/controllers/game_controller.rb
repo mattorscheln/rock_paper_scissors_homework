3605f71db4295f5441d27b169d9052882ee92cdd
mattorscheln <mjorsche@gmail.com>
2015-07-27 21:52:17 -0500

    HW5_rps_partone_COMPLETE

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,6 +4,35 @@ class GameController < ApplicationController
     redirect_to("/mockup.html")
   end
 
+  def rps
+
+	@user_move = params["the_move"]
+
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @user_move == @computer_move
+      @outcome = "tied"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @outcome = "won"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @outcome = "lost"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @outcome = "lost"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @outcome = "won"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @outcome = "lost"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @outcome = "won"
+    end
+
+
+
+
+    render("move.html.erb")
+
+  end
+
   # Your code goes below.
 
 end
