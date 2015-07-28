5bbbab935ecbc741681ef26bb1d53f44b9ec0ce7
mattorscheln <mjorsche@gmail.com>
2015-07-28 18:57:56 -0500

    HW5_rps_prepurge

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -26,13 +26,32 @@ class GameController < ApplicationController
       @outcome = "won"
     end
 
+# Adding an entry to the Move table for this turn
+    m = Move.new
+    m.user_move = @user_move
+    m.computer_move = @computer_move
+    if @outcome == "won"
+      m.user_wins = 1
+      m.computer_wins = 0
+      m.tie = 0
+    elsif @outcome == "lost"
+      m.user_wins = 0
+      m.computer_wins = 1
+      m.tie = 0
+    else
+      m.user_wins = 0
+      m.computer_wins = 0
+      m.tie = 1
+    end
 
+    m.save
 
+    # Retrieving all past moves in order to draw the log
+    @all_moves = Move.all
 
     render("move.html.erb")
-
   end
 
-  # Your code goes below.
+
 
 end
